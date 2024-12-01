a9152be4c198df0c055051239a880e6c1342791e
gitdrik <58667795+gitdrik@users.noreply.github.com>
2024-12-01 16:34:33 +0100

    Add files via upload

--- 01.jl
+++ 01.jl
@@ -3,7 +3,6 @@ open("01.txt") do f
     M = readdlm(f, Int)
     A, B = sort(M[:,1]), sort(M[:,2])
     println("Part 1: ", sum(abs.(A-B)))
-
     C = counter(B)
     println("Part 2: ", sum(a*C[a] for a∈A))
 end
