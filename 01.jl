ce5a32543c9899ef9ef50167e7affedf70791520
gitdrik <58667795+gitdrik@users.noreply.github.com>
2024-12-01 11:35:18 +0100

    Add files via upload

--- 01.jl
+++ 01.jl
@@ -1,17 +1,9 @@
+using Counters
 open("01.txt") do f
-    A::Array{Int}, B::Array{Int} = [], []
-    for l ∈ eachline(f)
-        a, b = parse.(Int, split(l))
-        push!(A, a)
-        push!(B, b)
-    end
-    sort!(A)
-    sort!(B)
-    println("Part 1: ", sum(abs.(reduce.(-, zip(A, B)))))
-    
-    p2 = 0
-    for a∈A, b∈B
-        a==b && (p2 += a)
-    end
-    println("Part 2: ", p2)
+    L = readlines(f) .|> split .|> ss->parse.(Int, ss)
+    A, B = sort(first.(L)), sort(last.(L))
+    println("Part 1: ", sum(abs.(A.-B)))
+
+    BC = counter(B)
+    println("Part 2: ", sum(a*BC[a] for a∈A))
 end
