0dcbe4f766886bf3cd471d97f9afeb11c081049a
gitdrik <58667795+gitdrik@users.noreply.github.com>
2024-12-01 11:40:20 +0100

    Add files via upload

--- 01.jl
+++ 01.jl
@@ -2,7 +2,7 @@ using Counters
 open("01.txt") do f
     L = readlines(f) .|> split .|> ss->parse.(Int, ss)
     A, B = sort(first.(L)), sort(last.(L))
-    println("Part 1: ", sum(abs.(A.-B)))
+    println("Part 1: ", sum(abs.(A-B)))
 
     BC = counter(B)
     println("Part 2: ", sum(a*BC[a] for a∈A))
