5f8094393a5fa15dec4d9e7ebc6fc36e606e5cf2
gitdrik <58667795+gitdrik@users.noreply.github.com>
2024-12-02 16:46:18 +0100

    Add files via upload

--- 02.jl
+++ 02.jl
@@ -1,15 +1,12 @@
 open("02.txt") do f
-    function unsafe(ns)
-        delta = ns[1:end-1]-ns[2:end]
-        any(∉(1:3), abs.(delta)) || 
-            any(≠(sign(delta[end])), sign.(delta))
-    end
     p1, p2 = 0, 0
     for l ∈ eachline(f)
         ns = parse.(Int, split(l))
         for i ∈ 0:length(ns)
             nns = [ns[1:i-1]; ns[i+1:end]]
-            unsafe(nns) && continue
+            delta = nns[1:end-1]-nns[2:end]
+            (any(∉(1:3), abs.(delta)) ||
+                any(≠(sign(delta[end])), sign.(delta))) && continue
             p1 += i==0
             p2 += 1
             break
