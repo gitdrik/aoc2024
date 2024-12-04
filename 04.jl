eafd749b9c0547657486603a59d9b614efff63db
gitdrik <58667795+gitdrik@users.noreply.github.com>
2024-12-04 15:36:13 +0100

    Add files via upload

--- 04.jl
+++ 04.jl
@@ -1,23 +1,17 @@
 open("04.txt") do f
-    ls = readlines(f)
-    rows, cols = eachindex(ls), 1:length(ls[1])
-    M = fill(' ', rows.stop+3, cols.stop+3)
-    for (i, l)∈enumerate(ls), (j, c)∈enumerate(l)
-        M[i,j] = c
-    end
-    p1 = 0
+    M = stack(readlines(f), dims=1)
+    rows, cols = size(M)
+    MASX = Set(collect.(["MAS", "SAM"]))
     XMAS = Set(collect.(["XMAS", "SAMX"]))
-    for r∈rows, c∈cols
-        p1 += M[r, c:c+3] ∈ XMAS
-        p1 += M[r:r+3, c] ∈ XMAS
-        p1 += [M[r+i,c+i] for i∈0:3] ∈ XMAS
-        p1 += [M[r+3-i,c+i] for i∈0:3] ∈ XMAS
+    p1, p2 = 0, 0
+    for r∈1:rows, c∈1:cols
+        c+3≤cols && (p1 += M[r, c:c+3] ∈ XMAS)
+        r+3≤rows && (p1 += M[r:r+3, c] ∈ XMAS)
+        r+3≤rows && c+3≤cols && (p1 += [M[r+i,c+i] for i∈0:3] ∈ XMAS)
+        r+3≤rows && c+3≤cols && (p1 += [M[r+3-i,c+i] for i∈0:3] ∈ XMAS)
+        r+2≤rows && c+2≤cols && (p2 += [M[r+i,c+i] for i∈0:2] ∈ MASX && 
+                                       [M[r+2-i,c+i] for i∈0:2] ∈ MASX)
     end
     println("Part 1: ", p1)
-    MASX = Set(collect.(["MAS", "SAM"]))
-    p2 = 0
-    for r∈rows, c∈cols
-        p2 += [M[r+i,c+i] for i∈0:2] ∈ MASX && [M[r+2-i,c+i] for i∈0:2] ∈ MASX
-    end
     println("Part 2: ", p2)
 end
