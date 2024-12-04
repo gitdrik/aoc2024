bea3c9f44ecc1f04cfdfda2821399c9192994c8b
gitdrik <58667795+gitdrik@users.noreply.github.com>
2024-12-04 08:15:37 +0100

    Add files via upload

--- 02.jl
+++ 02.jl
@@ -1,3 +1,4 @@
+function run()
 open("02.txt") do f
     p1, p2 = 0, 0
     for l ∈ eachline(f)
@@ -6,7 +7,7 @@ open("02.txt") do f
             nns = [ns[1:i-1]; ns[i+1:end]]
             delta = nns[1:end-1]-nns[2:end]
             any(∉(1:3), abs.(delta)) && continue
-            any(≠(sign(delta[end])), sign.(delta)) && continue
+            !allequal(sign.(delta)) && continue
             p1 += i==0
             p2 += 1
             break
@@ -14,4 +15,5 @@ open("02.txt") do f
     end
     println("Part 1: ", p1)
     println("Part 2: ", p2)
+end
 end
