c2cf8654710fa7a2b708c6914bbf9101cd4ccdfa
gitdrik <58667795+gitdrik@users.noreply.github.com>
2024-12-04 15:46:22 +0100

    Add files via upload

--- 04.jl
+++ 04.jl
@@ -1,12 +1,13 @@
+function run()
 open("04.txt") do f
-    M = stack(readlines(f), dims=1)
+    M = stack(readlines(f))
     rows, cols = size(M)
     MASX = Set(collect.(["MAS", "SAM"]))
     XMAS = Set(collect.(["XMAS", "SAMX"]))
     p1, p2 = 0, 0
     for r∈1:rows, c∈1:cols
-        c+3≤cols && (p1 += M[r, c:c+3] ∈ XMAS)
-        r+3≤rows && (p1 += M[r:r+3, c] ∈ XMAS)
+        c+3≤cols && (p1 += M[r, c:c+3]∈XMAS)
+        r+3≤rows && (p1 += M[r:r+3, c]∈XMAS)
         r+3≤rows && c+3≤cols && (p1 += [M[r+i,c+i] for i∈0:3] ∈ XMAS)
         r+3≤rows && c+3≤cols && (p1 += [M[r+3-i,c+i] for i∈0:3] ∈ XMAS)
         r+2≤rows && c+2≤cols && (p2 += [M[r+i,c+i] for i∈0:2] ∈ MASX && 
@@ -14,4 +15,5 @@ open("04.txt") do f
     end
     println("Part 1: ", p1)
     println("Part 2: ", p2)
+end
 end
