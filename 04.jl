6b4ef28ae70070dc53882f0edfab2fc684dd20ac
gitdrik <58667795+gitdrik@users.noreply.github.com>
2024-12-04 17:29:50 +0100

    Add files via upload

--- 04.jl
+++ 04.jl
@@ -2,15 +2,15 @@ open("04.txt") do f
     M = stack(readlines(f))
     rows, cols = size(M)
     MASX = Set(collect.(["MAS", "SAM"]))
-    XMAS = Set(collect.(["XMAS", "SAMX"]))
+    MAS = Set(collect.(["XMAS", "SAMX"]))
     p1, p2 = 0, 0
     for r∈1:rows, c∈1:cols
         c+3≤cols && (p1 += M[r, c:c+3]∈XMAS)
         r+3≤rows && (p1 += M[r:r+3, c]∈XMAS)
-        r+3≤rows && c+3≤cols && (p1 += [M[r+i,c+i] for i∈0:3] ∈ XMAS)
+        r+3≤rows && c+3≤cols && (p1 += [M[r+i,  c+i] for i∈0:3] ∈ XMAS)
         r+3≤rows && c+3≤cols && (p1 += [M[r+3-i,c+i] for i∈0:3] ∈ XMAS)
-        r+2≤rows && c+2≤cols && (p2 += [M[r+i,c+i] for i∈0:2] ∈ MASX && 
-                                       [M[r+2-i,c+i] for i∈0:2] ∈ MASX)
+        r+2≤rows && c+2≤cols && (p2 += [M[r+i,  c+i] for i∈0:2] ∈ MAS && 
+                                       [M[r+2-i,c+i] for i∈0:2] ∈ MAS)
     end
     println("Part 1: ", p1)
     println("Part 2: ", p2)
