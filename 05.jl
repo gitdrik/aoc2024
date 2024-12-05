b8c212b59b61030c98fe6921184b30aaa7abe237
gitdrik <58667795+gitdrik@users.noreply.github.com>
2024-12-05 21:03:44 +0100

    Add files via upload

--- 05.jl
+++ 05.jl
@@ -11,15 +11,15 @@ open("05.txt") do f
     p1, p2 = 0, 0
     for u ∈ updates
         good = [u[1]]
-        for i ∈ 2:length(u)
+        for n ∈ u[2:end]
             pos = length(good)+1
-            for j ∈ 1:length(good)
-                if good[j] ∈ afters[u[i]]
-                    pos = j
+            for i ∈ 1:length(good)
+                if good[i] ∈ afters[n]
+                    pos = i
                     break
                 end
             end
-            insert!(good, pos, u[i])
+            insert!(good, pos, n)
         end
         u==good ? p1 += good[end÷2+1] : p2 += good[end÷2+1]
     end
