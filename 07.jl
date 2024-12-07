d0997c60613e466d83af87de236c022282b6e8d7
gitdrik <58667795+gitdrik@users.noreply.github.com>
2024-12-07 12:12:16 +0100

    Add files via upload

--- 07.jl
+++ 07.jl
@@ -6,8 +6,8 @@ open("07.txt") do f
             n > ans && return false
             i > length(ns) && return ans==n
             return verify(n+ns[i], i+1, p2) ||
-                   verify(n*ns[i], i+1, p2) ||
-                   p2 && verify(n * 10^ndigits(ns[i]) + ns[i], i+1, p2)
+                   verify(n*ns[i], i+1, p2) || p2 &&
+                   verify(n*10^ndigits(ns[i]) + ns[i], i+1, p2)
         end
         verify(n, 1, false) && (p1 += ans)
         verify(n, 1, true) && (p2 += ans)
