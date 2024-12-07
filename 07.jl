e76bc9b4abbd75894f7ebf5629ca73232d8caf91
gitdrik <58667795+gitdrik@users.noreply.github.com>
2024-12-07 10:30:32 +0100

    Add files via upload

--- 07.jl
+++ 07.jl
@@ -1,18 +1,19 @@
-open("07.txt") do f
-    function verify(ans, n, ns, i, p2)
-        n > ans && return false
-        i > length(ns) && return ans==n
-        return verify(ans, n+ns[i], ns, i+1, p2) ||
-               verify(ans, n*ns[i], ns, i+1, p2) ||
-               p2 && verify(ans, n * 10^ndigits(ns[i]) + ns[i], ns, i+1, p2)
-    end
-
+function run()
+open("07.txt") do f    
     p1, p2 = 0, 0
     for l ∈ eachline(f)
         ans, n, ns... = parse.(Int, split(l, r" |: "))
-        verify(ans, n, ns, 1, false) && (p1 += ans)
-        verify(ans, n, ns, 1, true) && (p2 += ans)
+        function verify(ans, n, i, p2)
+            n > ans && return false
+            i > length(ns) && return ans==n
+            return verify(ans, n+ns[i], i+1, p2) ||
+                   verify(ans, n*ns[i], i+1, p2) ||
+                   p2 && verify(ans, n * 10^ndigits(ns[i]) + ns[i], i+1, p2)
+        end
+        verify(ans, n, 1, false) && (p1 += ans)
+        verify(ans, n, 1, true) && (p2 += ans)
     end
     println("Part 1: ", p1)
     println("Part 2: ", p2)
+end
 end
