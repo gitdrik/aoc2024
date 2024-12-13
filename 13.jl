8a34eefd4f12aa7de76b2bd8a4459685228a1e15
gitdrik <58667795+gitdrik@users.noreply.github.com>
2024-12-13 19:02:17 +0100

    Add files via upload

--- 13.jl
+++ 13.jl
@@ -3,13 +3,13 @@ open("13.txt") do f
     p1, p2, n = 0, 0, 10000000000000
     for m ∈ machines
         ss = split(m, r"\+|,|=|\n")
-        x1, y1, x2, y2, x, y = parse.(Int, ss[2:2:end])
-        A = (y*x2 - y2*x) // (y1*x2 - y2*x1)
-        B = (x - x1*A) // x2
+        ax, ay, bx, by, x, y = parse.(Int, ss[2:2:end])
+        A = (y*bx - by*x) // (ay*bx - by*ax)
+        B = (x - ax*A) // bx
         1==denominator(A)==denominator(B) && (p1 += Int(3*A+B))
         x, y = x+n, y+n
-        A = (y*x2 - y2*x) // (y1*x2 - y2*x1)
-        B = (x - x1*A) // x2
+        A = (y*bx - by*x) // (ay*bx - by*ax)
+        B = (x - ax*A) // bx
         1==denominator(A)==denominator(B) && (p2 += Int(3*A+B))
     end
     println("Part 1: ", p1)
