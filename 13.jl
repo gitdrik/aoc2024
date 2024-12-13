7ee3699955b4f9a4a9ee9d3dcd773f8f09db9b5c
gitdrik <58667795+gitdrik@users.noreply.github.com>
2024-12-13 17:05:29 +0100

    Add files via upload

--- 13.jl
+++ 13.jl
@@ -1,25 +1,13 @@
 open("13.txt") do f
     machines = split(read(f, String), "\n\n")
-    p1, p2 = 0, 0
-    n = 10000000000000
+    p1, p2, n = 0, 0, 10000000000000
     for m ∈ machines
         ss = split(m, r"\+|,|=|\n")
         x1, y1, x2, y2, x, y = parse.(Int, ss[2:2:end])
-        try
-            A::Int = (y*x2 - y2*x) / (y1*x2 - y2*x1)
-            B::Int = (x - x1*A) / x2
-            p1 += 3*A + B
-        catch
-            nothing
-        end
-        x, y = x+n, y+n
-        try
-            A::Int = (y*x2 - y2*x) / (y1*x2 - y2*x1)
-            B::Int = (x - x1*A) / x2
-            p2 += 3*A + B
-        catch
-            nothing
-        end
+        ab1, ab2 = [x1 x2; y1 y2] \ [x, y], [x1 x2; y1 y2] \ [x+n, y+n]
+        abr1, abr2 = round.(Int, ab1), round.(Int, ab2)
+        sum(abs.(ab1 .- abr1)) < 0.01 && (p1 += 3*abr1[1] + abr1[2])
+        sum(abs.(ab2 .- abr2)) < 0.01 && (p2 += 3*abr2[1] + abr2[2])        
     end
     println("Part 1: ", p1)
     println("Part 2: ", p2)
