456a67140444ee6be8b5d66b16afd99550a9a42e
gitdrik <58667795+gitdrik@users.noreply.github.com>
2024-12-30 12:46:25 +0100

    Add files via upload

--- 22.jl
+++ 22.jl
@@ -2,26 +2,32 @@ open("22.txt") do f
     p1 = 0
     prune = 16777215
     seqs = Array{Array{Int}}([])
+    deltas = Array{Array{Int}}([])
     for n ∈ parse.(Int, eachline(f))
-        push!(seqs, [digits(n)[1]])
+        push!(seqs, [mod(n, 10)])
+        push!(deltas, [0])
+        old = mod(n, 10)
         for i ∈ 1:2000
             n = (n ⊻ n<<6) & prune
             n = (n ⊻ n>>5) & prune
             n = (n ⊻ n<<11) & prune
-            push!(seqs[end], digits(n)[1])
+            new = mod(n, 10)
+            push!(seqs[end], new)
+            push!(deltas[end], new-old)
+            old = new
         end
         p1 += n
     end
     println("Part 1: ", p1)
-
     bananas = zeros(Int, 80000)
-    for seq ∈ seqs
+    for (j, delta) ∈ enumerate(deltas)
         seen = falses(80000)
-        for i ∈ 1:length(seq)-4
-            delta = 40000 + sum((seq[i+1:i+4]-seq[i:i+3]) .* [4096, 256, 16, 1])
-            if !seen[delta]
-                seen[delta] = true
-                bananas[delta] += seq[i+4]
+        d = delta[2]<<8 + delta[3]<<4 + delta[4]
+        for i ∈ 1:length(delta)-4
+            d = (d - delta[i]<<12)<<4 + delta[i+4]
+            if !seen[d + 40000]
+                seen[d + 40000] = true
+                bananas[d + 40000] += seqs[j][i+4]
             end
         end
     end
